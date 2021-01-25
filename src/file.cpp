70deef604037633a6ab1d25ecf28f371eb9a9ee3
mdecourse <mdecourse@gmail.com>
2021-01-25 17:35:43 +0800

    add link stl function

--- src/file.cpp
+++ src/file.cpp
@@ -703,8 +703,10 @@ void SolveSpaceUI::UpgradeLegacyData() {
 bool SolveSpaceUI::LoadEntitiesFromFile(const Platform::Path &filename, EntityList *le,
                                         SMesh *m, SShell *sh)
 {
-    if(strcmp(filename.Extension().c_str(), "emn")==0) {
-        return LinkIDF(filename, le, m, sh);    
+    if(strcmp(filename.Extension().c_str(), "emn")==0) { 
+        return LinkIDF(filename, le, m, sh);
+    } else if(strcmp(filename.Extension().c_str(), "stl")==0) {
+        return LinkStl(filename, le, m, sh);  
     } else {
         return LoadEntitiesFromSlvs(filename, le, m, sh);
     }
